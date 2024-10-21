500c888eee349e2361792c37e6c8b71305aa274f
OmarZahrah <omar.zahrah01@gmail.com>
2024-10-21 10:33:35 +0300

    chore: set up initial file structure and basic project layout

---
+++ app/account/page.tsx
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: "Guest Area",
+};
+
+export default function page() {
+  return (
+    <div>
+      <h1>Account</h1>
+    </div>
+  );
+}
---
+++ app/cabins/page.tsx
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: "Cabins",
+};
+
+export default function page() {
+  return (
+    <div>
+      <h1>Cabins</h1>
+    </div>
+  );
+}
