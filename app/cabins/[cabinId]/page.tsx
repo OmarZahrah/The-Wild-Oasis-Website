deb27c01f5fb80e0c9f09e77487808b6a9599e94
OmarZahrah <omar.zahrah01@gmail.com>
2024-10-23 13:39:52 +0300

    feat: make dynamic pages static with generateStaticParams

--- app/cabins/[cabinId]/page.tsx
+++ app/cabins/[cabinId]/page.tsx
@@ -1,4 +1,4 @@
-import { getCabin } from "@/app/_lib/data-service";
+import { getCabin, getCabins } from "@/app/_lib/data-service";
 import { EyeSlashIcon, MapPinIcon, UsersIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
 
@@ -19,6 +19,13 @@ interface Cabin {
 type CabinProps = {
   params: Params;
 };
+
+export async function generateStaticParams() {
+  const cabins = await getCabins();
+
+  const ids = cabins.map((cabin) => ({ cabinId: String(cabin.id) }));
+  return ids;
+}
 
 export async function generateMetadata({ params }: CabinProps) {
   const { name } = await getCabin(params.cabinId);
