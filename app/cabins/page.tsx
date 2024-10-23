83adab3c5c04838b53ccdff3e894c271cde3cfb2
OmarZahrah <omar.zahrah01@gmail.com>
2024-10-23 15:48:08 +0300

    fix: update caching settings and resolve various bugs

--- app/cabins/page.tsx
+++ app/cabins/page.tsx
@@ -6,6 +6,9 @@ export const metadata = {
   title: "Cabins",
 };
 
+// export const revalidate = 3600; //revalidate every  1 hour
+export const revalidate = 15;
+
 export default function Page() {
   return (
     <div>
