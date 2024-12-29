30f10f8afe95027302041301ca302a2c58934f59
OmarZahrah <omar.zahrah01@gmail.com>
2024-12-29 17:43:51 +0200

    fix: resolve import error for next-auth

--- app/_lib/auth.ts
+++ app/_lib/auth.ts
@@ -1,6 +1,6 @@
-import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 import { createGuest, getGuest } from "./data-service";
+import NextAuth from "next-auth";
 
 const authConfig = {
   providers: [
