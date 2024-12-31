c62a717c264a5de4104e336eb21270fadb5af884
OmarZahrah <omar.zahrah01@gmail.com>
2024-12-31 12:35:24 +0200

    make cabins page & cabin card responsive

--- app/_components/CabinList.tsx
+++ app/_components/CabinList.tsx
@@ -29,7 +29,7 @@ export default async function CabinList({ filter }: CabinListProps) {
   }
 
   return (
-    <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
+    <div className="grid sm:grid-cols-1 lg:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
       {displayCabins.map((cabin) => (
         <CabinCard cabin={cabin} key={cabin.id} />
       ))}
